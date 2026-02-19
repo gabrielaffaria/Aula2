35e0f0376c9ed1cf14401e2923d748cea232daee
gabrielaffaria <gabrielaffaria2009@gmail.com>
2026-02-19 11:33:54 -0300

    entregando Ex1, 2 e 3

---
+++ Exercicio2.cs
@@ -0,0 +1,13 @@
+// -- Arquivo Exercicio2.cs
+public static class Exercicio2
+{
+    //private static string nome = "Gabriela";
+    
+    public static void Executar()
+    {
+        string nome = "Gabriela";
+        Console.WriteLine(nome);
+        Console.WriteLine();
+    }
+}
+
