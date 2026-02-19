348c4fad36e0e646c438552b8c6024f205e1a3d7
gabrielaffaria <gabrielaffaria2009@gmail.com>
2026-02-19 16:35:44 -0300

    terminando exercicios

--- Exercicio4.cs
+++ Exercicio4.cs
@@ -1,15 +1,18 @@
-// -- Arquivo Exercicio3.cs
-using System.Reflection.Metadata.Ecma335;
+// -- Arquivo Exercicio4.cs
 using System.Runtime.InteropServices.Marshalling;
 
 public static class Exercicio4
 {
     public static void Executar()
     {
-       string nome = " Gabriela";
-       string hobby = "artes";
-       Console.WriteLine($"Meu nome é {nome} e meu hobby é {hobby}");
-       string idade = "Gabriela";
+       string nome = "Gabriela";
+       string idade = "16";
+       string hobby1 = "artes";
+       string hobby2 = "ler";
+       string curso = "psicologia";
+      Console.WriteLine($"Meu nome é {nome} e meu hobby é {hobby1}");
+      Console.WriteLine($"Meu hobby2 é {hobby2} e tenho idade {idade} e curso {curso}");
+
     }
 }
 
---
+++ Exercicio5.cs
@@ -0,0 +1,15 @@
+// -- arquivo exercicio5.cs
+using System.Runtime.InteropServices.Marshalling;
+
+public static class Exercicio5
+{
+   
+    public static void Executar()
+    {
+       string nome = "Gabriela";
+       string musica = "louvores";
+       string artista = "Fabio de melo";
+       
+       Console.WriteLine($"Eu sou {nome} , curto o estilo musical {musica} e meu artista favorito desse estilo é{artista}");
+    }
+}
