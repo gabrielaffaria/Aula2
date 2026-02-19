d69075feb829a20bcb8e11726859df8359c5e87e
gabrielaffaria <gabrielaffaria2009@gmail.com>
2026-02-19 14:31:14 -0300

    exercicio4.cs

---
+++ Exercicio4.cs
@@ -0,0 +1,17 @@
+// -- Arquivo Exercicio3.cs
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices.Marshalling;
+
+public static class Exercicio4
+{
+    public static void Executar()
+    {
+       string nome = " Gabriela";
+       string hobby = "artes";
+       Console.WriteLine($"Meu nome é {nome} e meu hobby é {hobby}");
+       string idade = "Gabriela";
+    }
+}
+
+
+
